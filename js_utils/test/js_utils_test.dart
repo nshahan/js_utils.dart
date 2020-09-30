9e5b69b2816e60a2883b06a886911f8bc9a5e4c1
alex <alex@tekartik.com>
2020-10-01 00:38:21 +0200

    test: comment out js_utils

--- js_utils/test/js_utils_test.dart
+++ js_utils/test/js_utils_test.dart
@@ -4,8 +4,9 @@ library tekartik_js_utils.test.js_utils_test;
 
 //import 'package:tekartik_js_utils/js_utils.dart';
 import 'package:dev_test/test.dart';
+//import 'package:test/test.dart';
 import 'package:js/js.dart';
-import 'package:js/js_util.dart';
+//import 'package:js/js_util.dart';
 
 /*
 import 'package:dev_test/test.dart';
